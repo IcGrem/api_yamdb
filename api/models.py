cad6fd0b01526eaf60ee46164d420c7f96b8c9d1
IcGrem <52701384+IcGrem@users.noreply.github.com>
2020-06-22 23:17:23 +0300

    Aleshin (#3)
    * Step 1: Categories and Genres, Titles are still in development
    
    * update models
    
    * delete permission.py
    
    * update serializers.py
    
    * update urls.py
    
    * update views.py
    
    Co-authored-by: Aleshin <aleshincc@yandex.ru>

---
+++ api/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+import datetime
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.PositiveSmallIntegerField(null=True)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(
+                                Category,
+                                on_delete=models.PROTECT,
+                                related_name="categories"
+                                )
+    genre = models.ManyToManyField(Genre, related_name="genres")
+
+    def __str__(self):
+        return self.name
+

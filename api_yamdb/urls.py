da7001453721a2823793716916b3bc50bb82daaf
Sergei Petukhov <Sergei.Petukhov@gmail.com>
2020-06-23 21:01:20 +0300

    main urls modify

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 
 urlpatterns = [
@@ -30,7 +26,5 @@ urlpatterns = [
     path('redoc/', TemplateView.as_view(
         template_name='redoc.html'),
         name='redoc'
-        ),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        )
 ]
